0e97b1d12a88531cc268d6dda05f7b31cdcb5d10
gottagoglo <Gloriann@Glorias-iMac.local>
2019-05-21 18:52:48 -0400

    add user and upload tables

--- models/user.js
+++ models/user.js
@@ -1,16 +1,10 @@
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    // Giving the User model a name of type STRING
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     name: DataTypes.STRING
-  });
-
+  }, {});
   User.associate = function(models) {
-    // Associating User with Statues
-    // When an User is deleted, also delete any associated Posts
-    User.hasMany(models.Statue, {
-      onDelete: "cascade"
-    });
+    // associations can be defined here
   };
-
   return User;
 };
