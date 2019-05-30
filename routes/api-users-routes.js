55bb91cea818fa7371c64ee8d7804857637c863e
BPresseisen <benpresseisen@gmail.com>
2019-05-29 20:04:10 -0400

    statueNameOnUploadPage

--- routes/api-users-routes.js
+++ routes/api-users-routes.js
@@ -1,6 +1,5 @@
 var db = require("../models");
 
-
 // POST route for saving a new user
 module.exports = function(app) {
   app.post("/api/users", function(req, res) {
@@ -13,22 +12,19 @@ module.exports = function(app) {
   });
 
   app.post("/api/uploads", function(req, res) {
-    
     var photo = req.body;
 
     //console.log(photo)
-    
+
     Object.keys(photo).forEach(key => {
-      //console.log('key123', key); 
-      var photoText = key    
-      console.log(photoText); 
+      //console.log('key123', key);
+      var photoText = key;
+      console.log(photoText);
       db.Upload.create({
         picture: photoText
       }).then(function(dbPost) {
         res.json(dbPost);
-      });    
+      });
+    });
   });
-    
-   
-   });
 };
