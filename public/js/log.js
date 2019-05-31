03c94526969b88455c5e42840d0835b74355d04a
Zach Hollingsworth <zahollingsworth@Zachs-MacBook.local>
2019-05-31 16:21:21 -0400

    modal added

--- public/js/log.js
+++ public/js/log.js
@@ -1,17 +1,60 @@
 $(document).ready(function() {
   console.log("hello world");
+  var image;
 
   getPhoto();
+
+  var imageSrc = [];
 
   function getPhoto() {
     $.get("/api/uploads", function(data) {
       console.log(data);
-    //   var rowsToAdd = [];
-    //   for (var i = 0; i < data.length; i++) {
-    //     rowsToAdd.push(createStatueRow(data[i]));
-    //   }
-    //   renderStatuesList(rowsToAdd);
-    //   nameInput.val("");
+      //var rowsPhotos = [];
+
+      //for (var i = 0; i < data.length; i++) {
+      // imageSrc += data[i].picture;
+      //     //console.log(imageSrc);
+      //   }
+
+      //   for (var j = 0; j<imageSrc.length; j++){
+      //     //var imageSrc2 = imageSrc[i].slice(22);
+      //     console.log("kasdhfkasjdhf=" + imageSrc2)
+      //   }
+
+      // var canvas = document.getElementById("c");
+      // var ctx = canvas.getContext("2d");
+
+      // var image = new Image();
+      // image.onload = function() {
+      //   ctx.drawImage(image, 0, 0);
+      // };
+
+      //   image = new Image();
+      var split = data[1].picture.split(",");
+
+      split = split.filter(function(str) {
+        return /\S/.test(str);
+    });
+
+    console.log(split)
+      //image.src =
+      var source = "data:image/png;base64, " + split[1];
+
+      
+
+      $(".image-div").prepend("<img id='theImg' src='" + source + "' />");
+
+      $('.image-div').prepend('<img id="theImg" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" />')
+
+      //console.log(img);
+
+      //$(".image-div").append(img);
+
+      //$("body").append(image);
+
+      //console.log(rowsPhotos);
+      //   renderStatuesList(rowsToAdd);
+      //   nameInput.val("");
     });
   }
 });
--- public/js/upload.js
+++ public/js/upload.js
@@ -1,127 +1,144 @@
 $(document).ready(function() {
   console.log("Hello World");
 
-  var imgSrc;
-  var image;
+  //   $('.modal').modal({
+  //     dismissible: false,
+  //     opacity: .5,
+  //     inDuration: 300,
+  //     outDuration: 200,
+  //     startingTop: '4%',
+  //     endingTop: '10%'
+  //   });
+});
 
-  ajaxCall();
+var imgSrc;
+var image;
 
-  function ajaxCall() {
-    //     // var listItemData = $(this)
-    //     //   .parent("td")
-    //     //   .parent("tr")
-    //     //   .data("author");
-    var id = window.location.pathname;
-    var number = [];
+ajaxCall();
 
-    for (var i = 8; i < id.length; i++) {
-      number += id[i];
+function ajaxCall() {
+  //     // var listItemData = $(this)
+  //     //   .parent("td")
+  //     //   .parent("tr")
+  //     //   .data("author");
+  var id = window.location.pathname;
+  var number = [];
+
+  for (var i = 8; i < id.length; i++) {
+    number += id[i];
+  }
+
+  console.log(number);
+  $.ajax({
+    method: "GET",
+    url: "/api/statues/" + number
+  }).then(function(dbPost) {
+    console.log(dbPost);
+    var map, infoWindow;
+    var statueName = dbPost.ASSET_NAME;
+    console.log(statueName);
+    $("#statueName").text(statueName);
+    var marker = {
+      lat: parseFloat(dbPost.LATITUDE),
+      lng: parseFloat(dbPost.LONGITUDE)
+    };
+    console.log(marker);
+    // The statue lat and lng would go into the marker variable
+    function initMap() {
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: marker,
+        zoom: 15,
+        distance: 100 + "km"
+      });
+      // radius is in meters
+      var circle = new google.maps.Circle({
+        map: map,
+        radius: 100,
+        fillColor: "#ff0000"
+      });
+      var pinPoint = new google.maps.Marker({ position: marker, map: map });
+      circle.bindTo("center", pinPoint, "position");
+      infoWindow = new google.maps.InfoWindow();
+      compareLocation();
     }
 
-    console.log(number);
-    $.ajax({
-      method: "GET",
-      url: "/api/statues/" + number
-    }).then(function(dbPost) {
-      console.log(dbPost);
-      var map, infoWindow;
-      var statueName = dbPost.ASSET_NAME;
-      console.log(statueName);
-      $("#statueName").text(statueName);
-      var marker = {
-        lat: parseFloat(dbPost.LATITUDE),
-        lng: parseFloat(dbPost.LONGITUDE)
-      };
-      console.log(marker);
-      // The statue lat and lng would go into the marker variable
-      function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-          center: marker,
-          zoom: 15,
-          distance: 100 + "km"
-        });
-        // radius is in meters
-        var circle = new google.maps.Circle({
-          map: map,
-          radius: 100,
-          fillColor: "#ff0000"
-        });
-        var pinPoint = new google.maps.Marker({ position: marker, map: map });
-        circle.bindTo("center", pinPoint, "position");
-        infoWindow = new google.maps.InfoWindow();
-        compareLocation();
-      }
+    function compareLocation() {
+      // Try HTML5 geolocation.
+      $("#camera--trigger").click(function() {
+        event.preventDefault();
+        imgSrc = $(".taken").attr("src");
+        console.log(imgSrc);
 
-      function compareLocation() {
-        // Try HTML5 geolocation.
-        $("button").click(function() {
-          event.preventDefault();
-          imgSrc = $(".taken").attr("src");
-          console.log(imgSrc);
-          
-          image = new Image();
-          console.log(image);
-          image.src = imgSrc;
-          $("body").append(image);
-          if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-              function(position) {
-                pos = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude
-                };
-                infoWindow.setPosition(pos);
-                infoWindow.setContent("Location found.");
-                infoWindow.open(map);
-                map.setCenter(pos);
-                console.log(pos);
-                console.log(marker);
-                // results are return in meters
-                var distance = google.maps.geometry.spherical.computeDistanceBetween(
-                  new google.maps.LatLng(marker.lat, marker.lng),
-                  new google.maps.LatLng(pos.lat, pos.lng)
-                );
-                console.log(distance);
-                if (distance <= 100) {
-                  alert("You Win");
-                  submitPost(imgSrc);
-                } else {
-                  alert("try again");
-                  submitPost(imgSrc);
-                  console.log(imgSrc.length);
-                }
-              },
-              function() {
-                handleLocationError(true, infoWindow, map.getCenter());
+        image = new Image();
+        console.log(image);
+        image.src = imgSrc;
+        $("body").append(image);
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            function(position) {
+              pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              };
+              infoWindow.setPosition(pos);
+              infoWindow.setContent("Location found.");
+              infoWindow.open(map);
+              map.setCenter(pos);
+              console.log(pos);
+              console.log(marker);
+              // results are return in meters
+              var distance = google.maps.geometry.spherical.computeDistanceBetween(
+                new google.maps.LatLng(marker.lat, marker.lng),
+                new google.maps.LatLng(pos.lat, pos.lng)
+              );
+              console.log(distance);
+              if (distance <= 100) {
+                $("#myModalRight").modal(open);
+                $("#image-divRight").prepend('<img id="theImg" src="' + imgSrc + '" />');
+
+                //alert("You Win");
+                submitPost(imgSrc);
+              } else {
+                $("#myModal").modal(open);
+                $("#image-div").prepend('<img id="theImg" src="' + imgSrc + '" />');
+
+                $("#map-div").append(pinPoint);
+                //alert("try again");
+
+                submitPost(imgSrc);
+                console.log(imgSrc.length);
               }
-            );
-          } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-          }
-        });
-      }
-      initMap();
+            },
+            function() {
+              handleLocationError(true, infoWindow, map.getCenter());
+            }
+          );
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      });
+    }
+    initMap();
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-          browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation."
-        );
-        infoWindow.open(map);
-      }
-    });
-  }
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(
+        browserHasGeolocation
+          ? "Error: The Geolocation service failed."
+          : "Error: Your browser doesn't support geolocation."
+      );
+      infoWindow.open(map);
+    }
+  });
+}
 
-  //submitPost(imgSrc);
+//submitPost(imgSrc);
 
-  //console.log(imgSrc);
+//console.log(imgSrc);
 
-  function submitPost(Upload) {
-    $.post("/api/uploads/", Upload, function() {
-      window.location.href = "/table";
-    });
-  }
-});
+function submitPost(Upload) {
+  $.post("/api/uploads/", Upload, function() {
+    //window.location.href = "/start.html";
+  });
+}
